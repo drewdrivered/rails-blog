22974833c8868611af76f1f2cd3e292fd0473584
Andrew Waller <drewdrivered@gmail.com>
2017-04-04 01:39:44 +0000

    initial blog commit

---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+class Ability
+  include CanCan::Ability
+
+ def initialize(user)
+   user ||= User.new # guest user (not logged in)
+   can :read, Post
+
+   can [:create, :update, :destroy], Post do |post|
+     post.user == user
+   end
+ end
+end
+
